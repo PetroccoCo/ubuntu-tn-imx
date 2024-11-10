259d80aec9b8daaa831d6d395778689c4976c628
Pete Winterscheidt <pete@petrocco.co>
2024-11-10 00:10:53 -0700

    Adding brcmfmac module support to the kernel. TODO add firmware bin.

--- kernel.mk
+++ kernel.mk
@@ -69,26 +69,30 @@ distclean: clean
 build: src
 	echo CONFIG_DRM_LEGACY=y >> $(KERNEL_DIR)/linux-tn-imx/arch/arm/configs/$(KERNEL_DEFCONFIG)
 	echo CONFIG_DRM_VIVANTE=y >> $(KERNEL_DIR)/linux-tn-imx/arch/arm/configs/$(KERNEL_DEFCONFIG)
+	echo CONFIG_BRCMUTIL=m >> $(KERNEL_DIR)/linux-tn-imx/arch/arm/configs/$(KERNEL_DEFCONFIG)
+	echo CONFIG_BRCMFMAC=m >> $(KERNEL_DIR)/linux-tn-imx/arch/arm/configs/$(KERNEL_DEFCONFIG)
+	echo CONFIG_BRCMFMAC_PROTO_BCDC=y >> $(KERNEL_DIR)/linux-tn-imx/arch/arm/configs/$(KERNEL_DEFCONFIG)
+	echo CONFIG_BRCMFMAC_SDIO=y >> $(KERNEL_DIR)/linux-tn-imx/arch/arm/configs/$(KERNEL_DEFCONFIG)
 	$(MAKE) ARCH=${ARCH} CROSS_COMPILE=${CC} -C $(KERNEL_DIR)/linux-tn-imx $(KERNEL_DEFCONFIG)
 	$(MAKE) ARCH=${ARCH} CROSS_COMPILE=${CC} -C $(KERNEL_DIR)/linux-tn-imx -j$(CPUS) all
 	$(MAKE) ARCH=${ARCH} CROSS_COMPILE=${CC} -C $(KERNEL_DIR)/linux-tn-imx -j$(CPUS) dtbs
 	$(MAKE) ARCH=${ARCH} CROSS_COMPILE=${CC} -C $(KERNEL_DIR)/linux-tn-imx -j$(CPUS) imx6qp-edm1-fairy-qca.dtb
 	$(MAKE) ARCH=${ARCH} CROSS_COMPILE=${CC} -C $(KERNEL_DIR)/linux-tn-imx -j$(CPUS) modules
 	$(MAKE) ARCH=${ARCH} CROSS_COMPILE=${CC} -C $(KERNEL_DIR)/linux-tn-imx -j$(CPUS) modules_install INSTALL_MOD_PATH=$(KERNEL_DIR)/linux-tn-imx/modules/
-	cd $(KERNEL_DIR)/qcacld-2.0; \
-	KERNEL_SRC=$(KERNEL_DIR)/linux-tn-imx CONFIG_CLD_HL_SDIO_CORE=y CONFIG_PER_VDEV_TX_DESC_POOL=1 SAP_AUTH_OFFLOAD=1 \
-	CONFIG_QCA_LL_TX_FLOW_CT=1 CONFIG_WLAN_FEATURE_FILS=y CONFIG_FEATURE_COEX_PTA_CONFIG_ENABLE=y \
-	CONFIG_QCA_SUPPORT_TXRX_DRIVER_TCP_DEL_ACK=y CONFIG_WLAN_WAPI_MODE_11AC_DISABLE=y TARGET_BUILD_VARIANT=user \
-	$(MAKE) ARCH=${ARCH} CROSS_COMPILE=${CC} -C $(KERNEL_DIR)/qcacld-2.0 -j$(CPUS)
-	KERNEL_SRC=$(KERNEL_DIR)/linux-tn-imx INSTALL_MOD_PATH=$(KERNEL_DIR)/linux-tn-imx/modules/ $(MAKE) ARCH=${ARCH} CROSS_COMPILE=${CC} -C $(KERNEL_DIR)/qcacld-2.0 -j$(CPUS) modules_install
+## 	cd $(KERNEL_DIR)/qcacld-2.0; \
+## 	KERNEL_SRC=$(KERNEL_DIR)/linux-tn-imx CONFIG_CLD_HL_SDIO_CORE=y CONFIG_PER_VDEV_TX_DESC_POOL=1 SAP_AUTH_OFFLOAD=1 \
+## 	CONFIG_QCA_LL_TX_FLOW_CT=1 CONFIG_WLAN_FEATURE_FILS=y CONFIG_FEATURE_COEX_PTA_CONFIG_ENABLE=y \
+## 	CONFIG_QCA_SUPPORT_TXRX_DRIVER_TCP_DEL_ACK=y CONFIG_WLAN_WAPI_MODE_11AC_DISABLE=y TARGET_BUILD_VARIANT=user \
+## 	$(MAKE) ARCH=${ARCH} CROSS_COMPILE=${CC} -C $(KERNEL_DIR)/qcacld-2.0 -j$(CPUS)
+## 	KERNEL_SRC=$(KERNEL_DIR)/linux-tn-imx INSTALL_MOD_PATH=$(KERNEL_DIR)/linux-tn-imx/modules/ $(MAKE) ARCH=${ARCH} CROSS_COMPILE=${CC} -C $(KERNEL_DIR)/qcacld-2.0 -j$(CPUS) modules_install
 
 src:
 	mkdir -p $(KERNEL_DIR)
 	if [ ! -f $(KERNEL_DIR)/linux-tn-imx/Makefile ] ; then \
 		curl -L $(KERNEL_ARCHIVE) | tar xz && \
 		mv linux-tn-imx* $(KERNEL_DIR)/linux-tn-imx ; \
-		curl -L $(QCACLD_ARCHIVE) | tar xz && \
-		mv qcacld-2.0* $(KERNEL_DIR)/qcacld-2.0 ; \
 	fi
+## 		curl -L $(QCACLD_ARCHIVE) | tar xz && \
+## 		mv qcacld-2.0* $(KERNEL_DIR)/qcacld-2.0 ; \
 
 .PHONY: build
